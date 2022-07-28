8e2a2add1252b8bc3cc71199a39613a13855a73b
Rudra <rudrasenr@gmail.com>
2022-07-28 08:05:48 +0600

    web.php change

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,20 @@
 <?php
-
 use App\Http\Controllers\Admin\Dashboard\DashboardController;
 
-use Illuminate\Support\Facades\Auth;
+//CUSTOMER CONTROLLERS
+use App\Http\Controllers\Customer\MapController;
+use App\Http\Controllers\Customer\CustomerController;
+
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return redirect()->route('login');
-});
+// CUSTOMER PANEl ROUTES
+Route::get('/', [MapController::class, 'index'])->name('home');
+Route::post('/book-saloon', [MapController::class, 'store'])->name('saloon.book')->middleware('auth');
+Route::post('/saloon-apply', [MapController::class, 'store'])->name('saloon.apply')->middleware('auth');
 
-Auth::routes([
-    'register' => false, // Registration Routes...
-    'reset' => false, // Password Reset Routes...
-    'verify' => false, // Email Verification Routes...
-]);
-// DASHBOARD
-Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+Route::get('/profile', [CustomerController::class, 'index'])->name('profile')->middleware('auth');
+Route::post('/profile/edit/{id}', [CustomerController::class, 'update'])->name('profile.update')->middleware('auth');
+Route::post('/profile/change-password/{id}', [CustomerController::class, 'update_password'])->name('profile.changePassword')->middleware('auth');
 
-Route::middleware('admin')->group(function () {
-    Route::prefix('admin')->as('admin.')->group(function () {
-
-    });
-});
+// ADMIN DASHBOARD ROUTES
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard')->middleware('auth');
